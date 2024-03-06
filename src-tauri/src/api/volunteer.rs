227dadd5233bb07faae16bbad98345a0905e4fbd
GustavoHSD <h.gustavospeed2002@gmail.com>
2024-03-06 18:51:57 -0300

    add database pool, fix volunteer.rs

--- src-tauri/src/api/volunteer.rs
+++ src-tauri/src/api/volunteer.rs
@@ -1,8 +1,7 @@
-use std::{i32, num::ParseIntError};
-
-use poem::{handler, http::StatusCode, web::{Data, Json, Path}, IntoResponse, Response};
-use sqlx::MySqlPool;
 use serde::{Serialize, Deserialize};
+use tauri::State;
+
+use crate::SqlitePoolWrapper;
 
 #[derive(Serialize)]
 pub struct Volunteer {
@@ -18,11 +17,10 @@ struct VolunteerReq {
     cpf: String,
 }
 
-#[handler]
 pub async fn create( 
-    pool: Data<&MySqlPool>,
-    req: Json<VolunteerReq>,
-    ) -> impl IntoResponse {
+    state: State<'_, SqlitePoolWrapper>,
+    req: VolunteerReq,
+    ) -> serde_json::Value {
     
     let query_result = sqlx::query!(
         "INSERT INTO Volunteer (name, cpf, is_active) values (?,?,?)",
@@ -30,71 +28,53 @@ pub async fn create(
         req.cpf,
         true
     )
-    .execute(pool.0)
+    .execute(&state.pool)
     .await;
      
-    let response = match query_result {
+    match query_result {
         Ok(query_result) => {
-            Json(serde_json::json!({
+            serde_json::json!({
                 "id": query_result.last_insert_id(),
                 "name": req.name,
-            })).into_response() 
+            })
         },
         Err(_) => { 
-            let error_message = serde_json::json!({"error": "Volunteer could not be inserted"}); 
-            Response::builder()
-            .status(StatusCode::INTERNAL_SERVER_ERROR)
-            .body(serde_json::to_string(&error_message).unwrap())
-            .into_response()
+            serde_json::json!({"error": "Volunteer could not be inserted"})
         }, 
     };
-    response
 }
 
-#[handler]
-pub async fn find_all(pool: Data<&MySqlPool>,) -> impl IntoResponse {
+pub async fn find_all(state: State<'_, SqlitePoolWrapper>,) -> serde_json::Value {
     let volunteers = sqlx::query_as!(
         Volunteer,
         "SELECT * FROM Volunteer"
     )
-    .fetch_all(pool.0)
+    .fetch_all(&state.pool)
     .await;  
 
-    let response = match volunteers {   
-        Ok(volunteers) => Json(volunteers).into_response(),
+    match volunteers {   
+        Ok(volunteers) => serde_json::to_string(&volunteers)?,
         Err(error) => {
-            let error_message = serde_json::json!({"error": error.to_string()}); 
-            Response::builder()
-            .status(StatusCode::INTERNAL_SERVER_ERROR)
-            .body(serde_json::to_string(&error_message).unwrap())
-            .into_response()
+            serde_json::json!({"error": error.to_string()}); 
         },
     }; 
-    response
 }
 
-#[handler]
-pub async fn find_by_id(Path(id): Path<i64>, pool: Data<&MySqlPool>) -> impl IntoResponse {
+pub async fn find_by_id(id: i64, state: State<'_, SqlitePoolWrapper>,) -> serde_json::Value {
     let volunteer = sqlx::query_as!(
         Volunteer,
         "SELECT * FROM Volunteer WHERE id = ?",
         id
     )
-    .fetch_one(pool.0)
+    .fetch_one(&state.pool)
     .await;
 
-    let response = match volunteer {
-        Ok(volunteer) => Json(volunteer).into_response(),
+    match volunteer {
+        Ok(volunteer) => serde_json::to_string(&volunteer).unwrap(),
         Err(_) => { 
-            let error_message = serde_json::json!({"message": "Volunteer not found"});
-            Response::builder()
-            .content_type("application/json; charset=utf-8")
-            .status(StatusCode::NOT_FOUND)
-            .body(serde_json::to_string(&error_message).unwrap())
-            .into_response() 
+            serde_json::json!({"message": "Volunteer not found"});
         }
     };
-    response
 }
 
 
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
+use tauri::Runtime;
+
 mod api;
 
+struct SqlitePoolWrapper {
+    pub pool: SqlitePool,
+}
+
+async fn establish_connection() -> SqlitePool {
+  dotenv::dotenv().expect("Unable to load environment variables from .env file");
+  
+  let db_url = std::env::var("DATABASE_URL").expect("Unable to read DATABASE_URL env var");
+  
+  SqlitePoolOptions::new()
+    .max_connections(100)
+    .connect(&db_url)
+    .await.expect("Unable to connect to Sqlite")
+}
+
 #[tokio::main]
-fn main() {
+async fn main() {
+    let pool: SqlitePool = Runtime::new().unwrap().block_on(establish_connection());
     tauri::Builder::default()
+        .manage(SqlitePoolWrapper{pool})
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
